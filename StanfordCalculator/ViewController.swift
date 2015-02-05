0640b705eb1ce883333d369bdc2de5ba0e7e928e
cliff538 <cliff538@yahoo.com>
2015-02-04 18:23:48 -0700

    Returned to ViewController.swift name, had problems.

---
+++ StanfordCalculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StanfordCalculator
+//
+//  Created by Cliff Gurske on 2/4/15.
+//  Copyright (c) 2015 cat3Development. All rights reserved.
+//
+
+import UIKit
+
+// inherits from SuperClass (in blue) UIViewController.
+class ViewController: UIViewController {
+
+	// instance variable
+	@IBOutlet weak var display: UILabel!
+
+	// method or function
+	@IBAction func appendDigit(sender: UIButton) {
+	}
+
+}
+
